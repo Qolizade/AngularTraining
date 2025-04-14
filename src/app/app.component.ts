46612b9a84f39f2245460bdd678f206717f87eef
Qolizade <qolizade151@gmail.com>
2025-04-14 16:43:24 +0330

    currency pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TitleCasePipe , DatePipe } from '@angular/common';
+import { TitleCasePipe, DatePipe, CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet , TitleCasePipe , DatePipe], //اضافه کن
+  imports: [RouterOutlet, TitleCasePipe, DatePipe, CurrencyPipe], //اضافه کن
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Pipes';
-  nameInput : string = '';
-  dateInput : string = '';
+  nameInput: string = '';
+  dateInput: string = '';
+  currencyInput: string = '';
 
   onNameChange(event: Event) {
     const Input = event.target as HTMLInputElement;
     this.nameInput = Input.value;
   }
 
-  onDateChange(event:Event){
+  onDateChange(event: Event) {
     const Input = event.target as HTMLInputElement;
     this.dateInput = Input.value;
+  }
+
+  onCurrencyChange(event:Event){
+    const Input = event.target as HTMLInputElement;
+    this.currencyInput = Input.value;
   }
 }
