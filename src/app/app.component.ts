672aa3dcd49e2d99b78079ef27cf6da553d0506d
Qolizade <qolizade151@gmail.com>
2025-04-14 16:29:56 +0330

    complete date pipe and titleCase pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TitleCasePipe , DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet , TitleCasePipe , DatePipe], //اضافه کن
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'AngularTraining';
+  title = 'Pipes';
+  nameInput : string = '';
+  dateInput : string = '';
+
+  onNameChange(event: Event) {
+    const Input = event.target as HTMLInputElement;
+    this.nameInput = Input.value;
+  }
+
+  onDateChange(event:Event){
+    const Input = event.target as HTMLInputElement;
+    this.dateInput = Input.value;
+  }
 }
